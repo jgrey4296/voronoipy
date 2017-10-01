be18d9cf49d4eb4e9ac66c327b9db67e550663b2
John Grey <jgrey@ucsc.edu>
2017-10-01 13:10:32 +0100

    added some modification experiments and debug checks

--- modify.py
+++ modify.py
@@ -1,3 +1,4 @@
+import numpy as np
 from cairo_utils.dcel import DCEL
 from cairo_utils.dcel.constants import FaceE, EdgeE, VertE
 from numpy.random import choice, sample, random
@@ -7,6 +8,7 @@ import IPython
 import cairo_utils as utils
 import pickle
 import random as rnd
+from itertools import cycle
 
 DCEL_PICKLE = "dcel.pkl"
 save_string = join("imgs", "v_modified")
@@ -28,40 +30,109 @@ surface, ctx, size, N = utils.drawing.setup_cairo(N=N)
 
 def modify(dcel):
     faces = {x.index : x for x in dcel.faces}
-    for face in dcel.faces:
-        face.data[FaceE.NULL] = True
+    chosen_face = choice(list(faces.values()))
+    #chosen_face = faces[384]
+    logging.info("Chosen face: {}".format(chosen_face.index))
+    # for face in dcel.faces:
+    #     face.data[FaceE.NULL] = True
 
-    face = choice(dcel.faces, 1)[0]
-    c = random(4)
-    face.data[FaceE.FILL] = c
-    face.data[FaceE.TEXT] = face.index
-    del face.data[FaceE.NULL]
+    #del chosen_face.data[FaceE.NULL]
 
-    for e in face.innerComponents:
-        if e.face is not None:
-            if FaceE.NULL in e.face.data:
-                del e.face.data[FaceE.NULL]
-            c = random(4)
-            e.face.data[FaceE.FILL] = c
+    for edge in dcel.halfEdges:
+        edge.data[EdgeE.NULL] = True
 
-    face2 = choice(face.innerComponents, 1)[0].face
-    for e in face2.innerComponents:
-        if e.face is not None:
-            if FaceE.NULL in e.face.data:
-                del e.face.data[FaceE.NULL]
-            c = random(4)
-            e.face.data[FaceE.FILL] = c
-                
+    colours = [
+        [1, 0, 0, 0],
+        [0.5, 0.5, 0, 0],
+        [0, 1, 0, 0],
+        [0, 0.5, 0.5, 0],
+        [0, 0, 1, 0],
+        [0.5, 0, 0.5, 0]
+        ]
+
+        
+    best = find_closest_faces(dcel, n=10)
+    totalEdges = 0
+
+    interior, exterior = get_edges_for_faces(dcel, best)
+    alreadyDealtWith = set()
+    for edge in interior:
+        assert(edge in dcel.halfEdges)
+        if EdgeE.NULL in edge.data:
+            del edge.data[EdgeE.NULL]
+        edge.data[EdgeE.COLOUR] = [0, 1, 0, 1]
+    for edge in exterior:
+        assert(edge in dcel.halfEdges)
+        if EdgeE.NULL in edge.data:
+            del edge.data[EdgeE.NULL]
+        edge.data[EdgeE.COLOUR] = [0, 0.5, 0.5, 1]
+
+    for face in best:
+        face.data[FaceE.FILL] = True
+        
+        
+
+        
+    # face = choice(dcel.faces, 1)[0]
+    # c = random(4)
+    # face.data[FaceE.FILL] = c
+    # face.data[FaceE.TEXT] = face.index
+    # del face.data[FaceE.NULL]
+
+    # for e in face.outerBoundaryEdges:
+    #     if e.face is not None:
+    #         if FaceE.NULL in e.face.data:
+    #             del e.face.data[FaceE.NULL]
+    #         c = random(4)
+    #         e.face.data[FaceE.FILL] = c
+
+    # face2 = choice(face.outerBoundaryEdges, 1)[0].face
+    # for e in face2.outerBoundaryEdges:
+    #     if e.face is not None:
+    #         if FaceE.NULL in e.face.data:
+    #             del e.face.data[FaceE.NULL]
+    #         c = random(4)
+    #         e.face.data[FaceE.FILL] = c
+
+    # dcel.constrain_to_circle(np.array((0.5, 0.5)), 0.45)
+            
     # for edge in dcel.halfEdges:
     #     edge.data[EdgeE.NULL] = True
     
     # for vert in dcel.vertices:
     #     vert.data[VertE.NULL] = True
-                  
+
+
+def find_closest_faces(dcel, point=np.array((0.5, 0.5)), n=5):
+    """ Return the N closest faces to a specified position """
+    logging.info("Finding closest faces")
+    assert(isinstance(dcel, DCEL))
+    assert(isinstance(point, np.ndarray))
+    assert(isinstance(n, int))
+    distances = [utils.math.get_distance(x.site, point) for x in dcel.faces]
+    paired = list(zip(distances, dcel.faces))
+    paired.sort()
+    best = paired[:n]
+    return [x[1] for x in best]
+
+def get_edges_for_faces(dcel, faces):
+    """ Return a tuple of (interior, exterior) edges given a group of faces """
+    logging.info("Getting edges for faces")
+    assert(isinstance(dcel, DCEL))
+    assert(isinstance(faces, list))
+    assert(all([isinstance(x, utils.dcel.Face) for x in faces]))
+    faceSet = set(faces)
+    allEdges = set([e for x in faces for e in x.edgeList] + [e for x in faces for e in x.outerBoundaryEdges])
+    interior_edges = [e for e in allEdges if e.face in faceSet and e.twin.face in faceSet]
+    exterior_edges = [e for e in allEdges if e.face in faceSet and e.twin.face not in faceSet]
+    return (interior_edges, exterior_edges)
+
+
 
 if __name__ == "__main__":
     the_dcel = DCEL.loadfile(DCEL_PICKLE)
+    utils.drawing.clear_canvas(ctx)
     modify(the_dcel)
-    utils.clear_canvas(ctx)
-    utils.drawDCEL(ctx, the_dcel, edges=True)
-    utils.write_to_png(surface, save_string)
+    
+    utils.dcel.drawing.drawDCEL(ctx, the_dcel, edges=True)
+    utils.drawing.write_to_png(surface, save_string)
